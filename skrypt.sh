57eccc14027e6600832522f1367811d88111fc24
Grzegorz Guz <dsw54400@dsw.edu.pl>
2024-07-08 11:07:22 +0200

    dodano funkcję tworzenia plikow logx.txt

--- skrypt.sh
+++ skrypt.sh
@@ -2,6 +2,18 @@
 
 if [[ "$1" == "--date" ]]; then
     date
+elif [[ "$1" == "--logs" ]]; then
+    num_files=100
+    if [[ -n "$2" ]]; then
+        num_files=$2
+    fi
+    for ((i=1; i<=num_files; i++))
+    do
+        filename="log${i}.txt"
+        echo "File name: $filename" > $filename
+        echo "Created by: $0" >> $filename
+        echo "Date: $(date)" >> $filename
+    done
 else
-    echo "Usage: skrypt.sh --date"
+    echo "Usage: skrypt.sh --date | --logs [num_files]"
 fi
