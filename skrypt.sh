3ba4beec9c2e07b2fb6dac15be299aca5a5e7303
Grzegorz Guz <dsw54400@dsw.edu.pl>
2024-07-10 10:26:28 +0200

    dodano obsługę flag oraz mod .gitignore

--- skrypt.sh
+++ skrypt.sh
@@ -1,28 +1,52 @@
 #!/bin/bash
 
 show_help() {
-    echo "Usage: skrypt.sh [OPTION]"
-    echo "--date        Display the current date"
-    echo "--logs [NUM]  Create NUM log files (default: 100)"
-    echo "--help        Display this help message"
+    echo "Dostępne opcje:"
+    echo "--date, -d        Wyświetla dzisiejszą datę"
+    echo "--logs, -l [N]    Tworzy N plików logx.txt, domyślnie 100"
+    echo "--help, -h        Wyświetla pomoc"
+    echo "--init            Klonuje repozytorium i ustawia PATH"
+    echo "--error, -e [N]   Tworzy N plików errorx/errorx.txt, domyślnie 100"
 }
 
-if [[ "$1" == "--date" ]]; then
-    date
-elif [[ "$1" == "--logs" ]]; then
-    num_files=100
-    if [[ -n "$2" ]]; then
-        num_files=$2
-    fi
+create_logs() {
+    num_files=${1:-100}
     for ((i=1; i<=num_files; i++))
     do
-        filename="log${i}.txt"
-        echo "File name: $filename" > $filename
-        echo "Created by: $0" >> $filename
-        echo "Date: $(date)" >> $filename
+        echo "log$i.txt" > "log$i.txt"
+        echo "Nazwa skryptu: $0" >> "log$i.txt"
+        date >> "log$i.txt"
     done
-elif [[ "$1" == "--help" ]]; then
-    show_help
-else
-    echo "Invalid option. Use --help for usage information."
-fi
+}
+
+create_errors() {
+    num_files=${1:-100}
+    for ((i=1; i<=num_files; i++))
+    do
+        mkdir -p "error$i"
+        echo "error$i.txt" > "error$i/error$i.txt"
+    done
+}
+
+case "$1" in
+    --date|-d)
+        date
+        ;;
+    --logs|-l)
+        create_logs $2
+        ;;
+    --help|-h)
+        show_help
+        ;;
+    --init)
+        git clone https://github.com/Pacmangol/narzedzia-lab4.git
+        export PATH=$PATH:$(pwd)/narzedzia-lab4
+        ;;
+    --error|-e)
+        create_errors $2
+        ;;
+    *)
+        echo "Nieznana opcja: $1"
+        show_help
+        ;;
+esac
